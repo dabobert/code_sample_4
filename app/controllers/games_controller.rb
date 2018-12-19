241bbac0300532c1e1bf8a47e6f4406f6dd83570
Robert Jenkins <bobert@gmail.com>
2018-12-19 15:33:01 -0500

    NOW /games post works

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,7 +4,7 @@ class GamesController < ApplicationController
   def create
     begin
       game = Game.create!
-      render :json => game
+      render :json => game.to_json
     rescue Exception => error
       render :json => {:status => 500, :message => error.message, :error => error.message}
     end
